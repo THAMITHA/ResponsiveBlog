2b4505634fd333039c521b6e008b94993108c75f
Thamitha <thamitharajput@gmail.com>
2023-06-02 04:15:15 +0530

    add logout functionality

--- firebase-blog-app/src/App.js
+++ firebase-blog-app/src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import "./style.scss";
 import "./media-query.css";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Detail from "./pages/Detail";
@@ -12,12 +12,40 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Auth from "./pages/Auth";
+import { auth } from "./firebase";
+import { signOut } from "firebase/auth";
 
 function App() {
   const [active, setActive] = useState("home");
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      setUser(null);
+      setActive("login");
+      navigate("/auth");
+    });
+  };
+
   return (
     <div className="App">
-      <Header setActive={setActive} active={active} />
+      <Header
+        setActive={setActive}
+        active={active}
+        user={user}
+        handleLogout={handleLogout}
+      />
       <ToastContainer position="top-center" />
       <Routes>
         <Route path="/" element={<Home />} />
